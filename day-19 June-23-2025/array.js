88b294aea8461b07cfa852a257f45e4a603d5d00
asif-daffodil <abir@dipti.com.bd>
2025-06-23 12:25:47 +0600

    61320251225pm

--- day-19 June-23-2025/array.js
+++ day-19 June-23-2025/array.js
@@ -11,10 +11,33 @@ for (let i = 0; i < studentNames.length; i++) {
 // shift
 // unshift
 // indexOf
+// splice
+// slice
+// reverse
+// split
 
 studentNames.push("Rafi", "Rifat");
 studentNames.pop();
 studentNames.unshift("Sagar", "Sagarika", "Arafa");
 studentNames.shift();
-
+console.log(studentNames.indexOf("Sagarika"));
 console.log(studentNames);
+studentNames.splice(2, 1, "Pagarika");
+console.log(studentNames);
+
+// slice
+const newStudentNames = studentNames.slice(1, 3);
+console.log(newStudentNames);
+
+// reverse
+const reversedNames = studentNames.reverse();
+console.log(reversedNames);
+
+// split
+const sentence = "I love programming";
+const words = sentence.split(" ");
+console.log(words);
+
+// join
+const joinedSentence = words.join(" ");
+console.log(joinedSentence);
