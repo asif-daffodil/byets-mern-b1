fd9a1ae8a042c2e750cd18ed6575f834f0130f89
asif-daffodil <abir@dipti.com.bd>
2025-08-06 11:40:24 +0600

    Add bcrypt dependency and implement password hashing API; update navigation for active page highlighting and create about page

--- day-46 Aug-06-2025/router/api.js
+++ day-46 Aug-06-2025/router/api.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const becript = require('bcrypt');
 
 router.get('/', (req, res) => {
     res.send('API is working');
+});
+
+router.get('/hash/:pass', (req, res) => {
+    const password = req.params.pass;
+    becript.hash(password, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).send('Error hashing password');
+        }
+        res.send(`Hashed password: ${hash}`);
+    });
 });
 
 module.exports = router;
--- day-46 Aug-06-2025/router/web.js
+++ day-46 Aug-06-2025/router/web.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Welcome to the Web Page' });
+    res.render('index', { title: 'Welcome to the Web Page', pageName: 'home' });
 });
 
+router.get('/about', (req, res) => {
+    res.render('about', { title: 'About Us', pageName: 'about' });
+});
 
 module.exports = router;
