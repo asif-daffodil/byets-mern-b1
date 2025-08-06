a0ac84ce9ab86d58fece1c6e15a8f9b5ea353d71
asif-daffodil <abir@dipti.com.bd>
2025-08-06 10:37:00 +0600

    Add initial API and web routes, along with EJS views
    - Created api.js for handling API requests with a simple response.
    - Created web.js for rendering the main web page with a welcome message.
    - Added header.ejs for the HTML structure and navigation bar.
    - Added footer.ejs for closing HTML tags and including Bootstrap scripts.
    - Created index.ejs to display a "Hello World!" message and include header and footer.

---
+++ day-46 Aug-06-2025/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(cors({
+    origin: '*',
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+}));
+
+app.use(express.static('public'));
+
+const apiRouter = require('./router/api');
+app.use('/api', apiRouter);
+
+const webRouter = require('./router/web');
+app.use('/', webRouter);
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
